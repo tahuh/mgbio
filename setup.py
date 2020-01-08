2047f3e7fda818006aef11054edbdd733c3f46de
tahuh <2011133049y@gmail.com>
2020-01-08 22:28:53 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name = "mgbio",
+        version = "0.0.1-beta",
+        description="Some building blocks for HTS data management",
+        author = "Thomas Sunghoon Heo",
+        author_email = "2011133049y@gmail.com",
+        url="https://github.com/tahuh/mgbio",
+        download_url = "",
+        install_requires= []
+)
